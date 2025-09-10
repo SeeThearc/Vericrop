381af6b65f4955012fd28d8caee758780b60cc2f
Abeer <abeergupta555@gmail.com>
2025-09-11 03:28:24 +0530

    add assets

---
+++ assets/general_dynamic_backround.jsx
@@ -0,0 +1,31 @@
+// npx shadcn@latest add https://www.shadcn.io/registry/bubble-background.json
+import { BubbleBackground } from "@/components/ui/bubble-background";
+
+export default function Hero() {
+  return (
+    <BubbleBackground interactive={true}>
+      <div className="relative z-10">
+        <h1>Your content here</h1>
+      </div>
+    </BubbleBackground>
+  );
+}
+
+
+
+
+
+// https://www.shadcn.io/background/circles
+// https://www.shadcn.io/background/fiber-waves
+// https://www.shadcn.io/background/grid-motion-background
+// https://www.shadcn.io/background/hexagon
+// https://www.shadcn.io/background/squares
+
+
+Effects
+// https://ui.aceternity.com/components/text-hover-effect
+// https://ui.aceternity.com/components/glowing-effect
+// https://ui.aceternity.com/components/background-ripple-effect
+// https://ui.aceternity.com/components/card-hover-effect
+
+
